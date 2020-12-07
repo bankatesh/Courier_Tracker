b718766c6bf28f9c182d8cbeba7b3071ada11ea0
bankatesh <55478612+bankatesh@users.noreply.github.com>
2020-12-07 12:57:23 +0530

    Update views.py

--- trackapp/views.py
+++ trackapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse ,JsonResponse
-import aftership
 import requests
 import trackingmore
 
@@ -13,6 +12,7 @@ trackingmore.set_api_key('1b17d38c-bbf4-4313-a71d-569b566bc08a')
 def InputForm(request):
     return render(request, 'trackapp/InputForm.html')
 
+
 def Result(request):
     name = request.GET.get('name')
     email = request.GET.get('emf')
@@ -23,10 +23,11 @@ def Result(request):
     for item in trackingmore.get_all_trackings()["data"]["items"]:
         if item["tracking_number"]==track_number1:
             trackingmore.delete_tracking_item(company, track_number1)
+            
     td= trackingmore.create_tracking_data(company,track_number1)
     track_data = trackingmore.realtime_tracking(td)
-    #return JsonResponse(track_data, safe=False)
     track_info1= track_data["items"][0]["origin_info"]["trackinfo"]
+    
     lis = []
     for item1 in track_info1:
         lis.append(item1)
@@ -41,8 +42,6 @@ def Result(request):
         'name':name,
     }
 
-
-   #context = { 'lis' :[1,2,3,5,7]}
 
     return render(request, 'trackapp/Result.html', context)
 
